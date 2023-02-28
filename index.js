64e72335465db175583d0a95b4b8b2334eb98e22
ahmed <ahmed.elmazry@gmail.com>
2023-02-28 22:17:07 +0200

    upload images folder

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const app = express();
 //Handle Cors and Middleware
 app.use((req, res, next) => {
   res
-    .header("Access-Control-Allow-Origin")
+    .header("Access-Control-Allow-Origin", "*")
     .header("Access-Control-Allow-Methods", "GET, POST, HEAD, PUT, DELETE")
     .header(
       "Access-Control-Allow-Headers",
