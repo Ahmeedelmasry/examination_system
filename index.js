5e47e25fd073b3e31552e4a6e8a0985e401762c8
ahmed <ahmed.elmazry@gmail.com>
2023-03-06 10:22:32 +0200

    update faveicon

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+// const cors = require("cors");
 
 //Routes
 const signupRoute = require("./routes/signup/signUp");
